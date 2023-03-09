d5f8cba245dd6c32717e46785397c0c2be395667
Mikael Monteiro <112489130+mikaelmonteirodev@users.noreply.github.com>
2023-03-09 00:34:38 -0300

    fix: update changes

--- script.jsx
+++ script.jsx
@@ -1,57 +1,66 @@
 function Profile(props){
-    return <div>
-       <h1>{props.nome}</h1>
-       <h2>{props.description}</h2>
-       
-       {props.children}
-    </div>;
- }
- function ProfileLinks(props){
-    const twitter = props.twitter;
-    const linkedin = props.linkedin;
-    return <div>
-      Twitter:  <a href={"https://twitter.com/" + twitter}> @{twitter}  </a>
-      <br />
-      Linkedin:  <a href={"https://linkedin.com/in/" + linkedin}> @{linkedin}</a>
-      <br />
- 
-    </div>;
- }
- function ProfileSkills(props){
-    const conteudo = props.children;
-    return <div className="pictures"> {conteudo}  </div>;
- }
- function JSSkill(){
-    return <img src="" alt="JS" />;
- }
- function Html5Skill() {
-    return <img src="" alt="Html" />;
- }
- function CSSSkill(){
-    return <img src="" alt="CSS" />;
- }
- function NodeSkill(){
-    return <img src="" alt="Node" />;
- }
- 
- function App() {
-    return   <Profile
-       nome="Mikael"
-       description=" lorem ipsum asdasda sda sdas as dasd asd  apoap a"  
-       >
-          <ProfileLinks 
-             twitter="mikael"
-             linkedin="mikaeldev"
-          />
- 
-          <ProfileSkills>
-             <JSSkill />
-             <Html5Skill />
-             <CSSSkill />
-             <NodeSkill />
-          </ProfileSkills>
-       </Profile>
- }
- 
- ReactDOM.render(<App />, document.getElementById('app'));
+   return <div id="profile">
+      <h1>{props.nome}</h1>
+      <p>{props.description}</p> <br />      
+      {props.children}
+   </div>;
+}
+
+function ProfileLinks(props){
+   const twitter = props.twitter;
+   const linkedin = props.linkedin;
+   const github = props.github;
+   return <div id="links">     
+     Github:  <a href={"https://github.com/" + github}> @{github} </a>
+     <br />
+     Linkedin:  <a href={"https://linkedin.com/in/" + linkedin}> @{linkedin} </a>
+     <br />
+     Twitter:  <a href={"https://twitter.com/" + twitter}> @{twitter} </a>
+     <br />     
+   </div>;  
+}
+
+function ProfileSkills(props){
+   const conteudo = props.children;
+   return <div className="pictures">{conteudo}</div>;
+}
+
+function JSSkill(){
+   return <img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/javascript-colored.svg" alt="JS" />;
+}
+
+function Html5Skill() {
+   return <img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/html5-colored.svg" alt="HTML" />;
+}
+
+function CSSSkill(){
+   return <img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/css3-colored.svg" alt="CSS" />;
+}
+
+function NodeSkill(){
+   return <img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/nodejs-colored.svg" alt="Node" />;
+}
+
+function App() {
+   return <Profile
+      nome="Mikael Monteiro"
+      description="👨‍💻 I'm a Full Stack development student at Digital College in Fortaleza | Ceará | Brazil 🤩 I love technology, music, comedy, my friends and family 🤔 I have a lot of ideas in my mind and I'm interested in collaborating with the developer community.">
+     
+         <ProfileLinks 
+            github="mikaelmonteirodev"
+            twitter="mikaelmonteiro"
+            linkedin="mikaelmonteiro"
+         />
+
+         <ProfileSkills>
+            <JSSkill />
+            <Html5Skill />
+            <CSSSkill />
+            <NodeSkill />
+         </ProfileSkills>
+      </Profile>
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
+
  
